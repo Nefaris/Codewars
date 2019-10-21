4cb1b22063224e050ef1bb3175eda406178ddf99
Nefaris <kamcio190@gmail.com>
2019-10-21 12:08:00 +0200

    Create TheSupermarketQueue.java

---
+++ src/TheSupermarketQueue.java
@@ -0,0 +1,15 @@
+public class TheSupermarketQueue {
+
+    public static void main(String[] args) {
+        System.out.println(solveSuperMarketQueue(new int[]{5, 3, 4}, 1) == 12);
+        System.out.println(solveSuperMarketQueue(new int[]{10, 2, 3, 3}, 2) == 10);
+        System.out.println(solveSuperMarketQueue(new int[]{2, 3, 10}, 2) == 12);
+    }
+
+    public static int solveSuperMarketQueue(int[] customers, int n) {
+        int result = 0;
+
+        return result;
+    }
+
+}
